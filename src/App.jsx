a1cc127add8d2244104235fc45edeaa847e78400
dahomeykid <dahomeykid@gmail.com>
2025-03-26 13:11:17 +0100

    added App Route and tailwing config

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 import './App.css'
 
 function App() {
 
   return (
-    <>
-     <h1>Hello App !!!</h1>
-      
-    </>
+    <Router>
+      <div className="min-h-screen bg-gray-100 p-5">
+        <h1 className="text-2xl font-bold text-center mb-4">To-Do App</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
